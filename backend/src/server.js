7964cb31c59c6673122dc94e371f3b34ec492465
Ujjwal092 <us581318@gmail.com>
2025-12-29 18:18:06 +0530

    change in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,12 +14,12 @@ app.use(express.json());
 //mounting
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-// const frontendPath = path.join(__dirname, "../frontend/dist");
-// console.log("Serving frontend from:", frontendPath);
+const frontendPath = path.join(__dirname, "../frontend/dist");
+console.log("Serving frontend from:", frontendPath);
 
 // make ready for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "./frontend/dist")));
 
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
