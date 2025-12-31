7707dd0f48cc762f29df6e60a4ad180b7329a31f
Ujjwal kumar <149806888+Ujjwal092@users.noreply.github.com>
2025-12-31 20:18:46 +0530

    Clean up auth.route.js by removing unused routes
    Removed commented-out login and logout routes.

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,14 +2,5 @@ import express from "express";
 import { signup } from "../controllers/auth.controller.js";
 //importing express to create router
 const router = express.Router();
-
-// router.get("/login", (req, res) => {
-//   res.send("Login endpoint");
-// });
-
-// router.post("/logout", (req, res) => {
-//   res.send("Logout endpoint");
-// });
-
 router.post("/signup", signup); //signup controller to be implemented
 export default router;
