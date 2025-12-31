7521d2c23f82b55c51f8b725877891171c8d6641
Ujjwal092 <us581318@gmail.com>
2026-01-01 00:38:53 +0530

    login and logout added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -73,3 +73,46 @@ export const signup = async (req, res) => {
 };
 
 //login controller
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //check if all fields are provided
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+    //check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    //compare password
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    //generate token
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+//logout controller
+export const logout = async (req, res) => {
+  try {
+    //clearing cookie by setting maxAge to 0
+    res.clearCookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "server error" });
+  }
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 //importing express to create router
 const router = express.Router();
 router.post("/signup", signup); //signup controller to be implemented
+router.post("/login", login); //login controller to be implemented
+router.post("/logout", logout); //logout controller to be implemented
 export default router;
