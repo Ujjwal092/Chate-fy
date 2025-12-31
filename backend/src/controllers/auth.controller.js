cdbbd3067a8612a799a08280ae27362e2df5e327
Ujjwal kumar <149806888+Ujjwal092@users.noreply.github.com>
2025-12-31 20:21:46 +0530

    Correct user save and token generation order
    Fix typo in variable declaration and reorder token generation.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -36,8 +36,8 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+     cosnt savedUser = await newUser.save();
+      generateToken(savedUser._id,res);
 
       return res.status(201).json({
         _id: newUser._id,
